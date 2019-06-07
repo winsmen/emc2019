4aaf0be63084c7d4be8e118b3f013c1448f017b7
Rompelberg, R.M.H <r.m.h.rompelberg@student.tue.nl>
2019-06-07 11:37:11 +0200

    Update src/localization.h

--- src/localization.h
+++ src/localization.h
@@ -7,55 +7,69 @@
 #include<math.h>
 
 struct Localization{
-    double tollarance=0.05;
-    double corner_x1=;  // From the Json file
-    double corner_x2=;  //From the Json file
-    double corner_x3=;  //From the Json file
-    double corner_x4=;  //From the Json file
-    double corner_y1=;  //From the Json file
-    double corner_y2=;  //From the Json file
-    double corner_y3=;  //From the Json file
-    double corner_y4=;  //From the Json file
-    double corner_x1_measured; //From the laser data
-    double corner_x2_measured; //From the laser data
-    double corner_x3_measured; //From the laser data
-    double corner_x4_measured; //From the laser data
-    double corner_y1_measured; //From the laser data
-    double corner_y2_measured; //From the laser data
-    double corner_y3_measured; //From the laser data
-    double corner_y4_measured; //From the laser data
-    double x_robot; //The robot initial x position
-    double y_robot; //The robot initial y position
-    
-    void localization(){
-        
-        double angle1=corner.0.i*scan.angle_increment;
-        double angle2=corner.1.i*scan.angle_increment;
-        double angle3=corner.2.i*scan.angle_increment;
-        double angle4=corner.3.i*scan.angle_increment;
-        
-        polar2cart(corner.0.d,angle1,corner_x1_measured,corner_y1_measured);
-        polar2cart(corner.1.d,angle2,corner_x2_measured,corner_y2_measured);
-        polar2cart(corner.2.d,angle3,corner_x3_measured,corner_y3_measured);
-        polar2cart(corner.3.d,angle4,corner_x4_measured,corner_y1_measured);
-        
-        double distance_corner12_measured=distance(corner_x1_measured,corner_y1_measured,corner_x2_measured,corner_y2_measured);
-        double distance_corner34_measured=distance(corner_x3_measured,corner_y3_measured,corner_x4_measured,corner_y4_measured);
-        double distance_corner12=distance(corner_x1,corner_y1,corner_x2,corner_y2);
-        double distance_corner34=distance(corner_x3,corner_y3,corner_x4,corner_y4);
-        
-    if (distance_corner12-distance_corner12_measured <= tollarance && distance_corner34 - distance_corner34_measured <= tollarance)
-    {
-        cout << "Exit is located" << endl;
-        x_robot=corner_x1-sin(angle1)*corner.0.d;
-        y_robot=corner_y1-cos(angle1)*corner.0.d;
-    }
-    else
-    {
-        cout <<"Exit is not located" << endl;
-    }
-    } 
+double Dist_tollerance=0.05;
+double corner_x1;  // From the Json file
+double corner_x2;  //From the Json file
+double corner_x3;  //From the Json file
+double corner_x4;  //From the Json file
+double corner_y1;  //From the Json file
+double corner_y2;  //From the Json file
+double corner_y3;  //From the Json file
+double corner_y4;  //From the Json file
+double corner_x1_measured; //From the laser data
+double corner_x2_measured; //From the laser data
+double corner_x3_measured; //From the laser data
+double corner_x4_measured; //From the laser data
+double corner_y1_measured; //From the laser data
+double corner_y2_measured; //From the laser data
+double corner_y3_measured; //From the laser data
+double corner_y4_measured; //From the laser data
+double x_robot; //The robot initial x position
+double y_robot; //The robot initial y position
+localization();
 };
+
+void localization()
+{
+    double angle1=World.convex_corners.0.i*scan.angle_increment;
+    double angle2=World.exits.leftEdge.i*scan.angle_increment;
+    double angle3=World.exits.rightEdge.i*scan.angle_increment;
+    double angle4=World.convex_corners.size().i*scan.angle_increment;
+    polar2cart(World.convex_corners.0.d,angle1,corner_x1_measured,corner_y1_measured);
+    polar2cart(World.exits.leftEdge.d,angle2,corner_x2_measured,corner_y2_measured);
+    polar2cart(World.exits.rightEdge.d,angle3,corner_x3_measured,corner_y3_measured);
+    polar2cart(World.convex_corners.size().d,angle4,corner_x4_measured,corner_y4_measured);
+    int j=0; 
+    double distance_corner12_measured=distance(corner_x1_measured,corner_y1_measured,corner_x2_measured,corner_y2_measured);
+    double distance_corner34_measured=distance(corner_x3_measured,corner_y3_measured,corner_x4_measured,corner_y4_measured);
+    double distance_corner12=distance(corner_x1,corner_y1,corner_x2,corner_y2);
+    double distance_corner34=distance(corner_x3,corner_y3,corner_x4,corner_y4);
+
+    while (abs(distance_corner12-distance_corner12_measured)>Dist_tollerance) {
+        j++;
+        polar2cart(World.convex_corners.j.d,angle1,corner_x1_measured,corner_y1_measured);
+        double distance_corner12_measured=distance(corner_x1_measured,corner_y1_measured,corner_x2_measured,corner_y2_measured);
+        int R=j;
+        if (j=>World.convex_corners.size().d){
+            cout<<"Corner 1 cannot be found"<<endl;
+            break;
+        }
+    }
+    
+    j=World.convex_corners.size().d;
+    
+    while (abs(distance_corner34-distance_corner34_measured)>Dist_tollerance) {
+        j--;
+        polar2cart(World.convex_corners.j.d,angle1,corner_x4_measured,corner_y4_measured);
+        double distance_corner34_measured=distance(corner_x3_measured,corner_y3_measured,corner_x4_measured,corner_y4_measured);
+        if (j<=0){
+            cout<<"Corner 4 cannot be found"<<endl;
+            break;
+        }
+    }
+    x_robot=corner_x1-sin(angle1)*corner.R.d;
+    y_robot=corner_y1-cos(angle1)*corner.R.d; 
+}
 
 #endif // LOCALIZATION_H
 
